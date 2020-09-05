9b53334d08d246d3ffde317ec141305533e85fdf
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-09-05 21:30:17 +0700

    feat: create book server

---
+++ REST API/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+import Book from "./Book.js";
+const bookData = new Book();
+
+app.use(bodyParser.json());
+
+app.get("/book/list", (req, res) => {
+  let books = bookData.getBookList();
+  res.json({ books });
+});
+
+app.get("/book/:bookId", (req, res) => {
+  let book = bookData.getBookById(req.params.bookId);
+  res.json({ book });
+});
+
+app.post("/book/insert", (req, res) => {
+  bookData.insertBook(req.body);
+  res.send();
+});
+
+app.delete("/book/:bookId", (req, res) => {
+  bookData.deleteBookById(req.params.bookId);
+  res.send();
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`);
+});
