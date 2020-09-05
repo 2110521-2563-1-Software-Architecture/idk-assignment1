a8c0fa5569e6c773c3d71b707e7b39aa6ed7541b
David Tumcharoen <d19450t@gmail.com>
2020-09-05 23:34:24 +0700

    Add swagger

--- REST API/index.js
+++ REST API/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json'
 
 const app = express();
 const PORT = 3000;
@@ -28,6 +30,8 @@ app.delete("/book/:bookId", (req, res) => {
   bookData.deleteBookById(req.params.bookId);
   res.send();
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`app listening at http://localhost:${PORT}`);
